5f25af3cbccc925355c8a2efb0e6f5649beef83f
boduga <boduga@gmail.com>
2025-04-23 15:02:20 -0600

    update langchainFuncs.py

--- langchainFuncs.py
+++ langchainFuncs.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain.chat_models import ChatGoogleGenerativeAI
-from langchain.schema import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 # Use your Gemini API key from Streamlit secrets
 api_key = st.secrets["GOOGLE_API_KEY"]
