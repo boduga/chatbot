1cd505296fc4585492445a07a60aaff00cad3959
Babasola <boduga@gmail.com>
2025-04-24 05:09:53 +0000

    refactor: update message handling and remove unused retrieval prompt in langchainFuncs2.py

--- langchainFuncs2.py
+++ langchainFuncs2.py
@@ -13,29 +13,34 @@ import pandas as pd
 
 
 df = pd.read_csv("100-trans.csv")
-print(df.head())
-
 
 llm = ChatGoogleGenerativeAI(
     model="models/gemini-2.5-flash-preview-04-17",
     api_key=st.secrets["GOOGLE_API_KEY"]
 )
 
-# prompts
-retrieval_prompt = PromptTemplate.from_template(
-    '''
-Based on the following input: '{input}', create the appropriate query to filter and return results from a python dataframe (df.query()).
-The dataframe contains the following columns: '{columns}'. Only return the query string without any additional text.
-'''
-)
-retrieval_prompt = retrieval_prompt.partial(
-    columns=(df.columns)
-)
+# # prompts
+# retrieval_prompt = PromptTemplate.from_template(
+# '''
+# Based on the following input: '{input_text}', create the appropriate query to filter and return results from a python dataframe (df.query()).
+# The dataframe contains the following columns: '{col_vals}'. Only return the query string without any additional text.'''
+# )
+# retrieval_prompt = retrieval_prompt.partial(col_vals=(df.columns))
 
-#chain
-chain = retrieval_prompt | llm | StrOutputParser()
+# # chain
+# chain = retrieval_prompt | llm | StrOutputParser()
+# # run
+# user_input = {"input_text": "Please give me  the total sales in the month of January."}
+# response = chain.invoke(user_input)
+# print(response)
 
-# run
-user_input = {"input": "What is the total revenue for each month?"}
-response = chain.invoke(user_input)
-print(response)
+
+
+messages = [
+    SystemMessage(content="You are an ecommerce database agent. [DATA]: " + df.values.tolist().__str__()),
+    HumanMessage(content="Please tell me the revenue figures for January?")
+]
+
+response = llm.invoke(messages)
+print("Full LLM Response: ", response)
+print("LLM Response Content: ", response.content)
